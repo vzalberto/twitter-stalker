6188080207cc1ad45c30b84c2b0e2dbc343e743b
vzalberto <vzalberto@msn.com>
2013-09-01 15:02:40 -0500

    Updated description

--- Stalker.py
+++ Stalker.py
@@ -1,5 +1,5 @@
 # Needs https://github.com/bear/python-twitter
-# Only works fine for small timelines. Consider Twitter's API rate limiting
+# Only works with for small timelines. Consider Twitter's API rate limiting
 
 def GetEntireUserTimeline(user):
 	stCount = api.GetUser(screen_name=user).statuses_count
